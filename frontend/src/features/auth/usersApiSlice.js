6c1ddad25186a9b0d2c43cd49250901cd7469d7c
marshall nickolauson <marshnickol101@gmail.com>
2024-10-23 07:11:57 -0400

    frontend redux setup for users

---
+++ frontend/src/features/auth/usersApiSlice.js
@@ -0,0 +1,27 @@
+import { apiSlice } from "../../api/apiSlice";
+const USERS_URL = 'http://localhost:5000/api/users';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/login`,
+                method: 'POST',
+                body: data,
+                credentials: 'include',
+            }),
+        }),
+        logout: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/logout`,
+                method: 'POST',
+                credentials: 'include'
+            })
+        })
+    })
+});
+
+export const {
+    useLoginMutation, // GET will be "useLoginQuery" for something like the profile (delete me)
+    useLogoutMutation,
+} = usersApiSlice;
