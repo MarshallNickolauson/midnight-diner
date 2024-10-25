f511478154cb3c7fa72d041d8dbb225beef82f87
marshall nickolauson <marshnickol101@gmail.com>
2024-10-25 07:26:05 -0400

    updated styling

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -8,8 +8,8 @@ const HeroSection = () => {
                 <h1 className='text-mainWhite mr-3'>WELCOME TO</h1>
                 <h1 className='text-mainYellow'>MIDNIGHT DINER</h1>
             </div>
-            <p className='flex justify-center tracking-wider text-mainWhite text-3xl font-oswald'>
-                Where late-night cravings meet irresistible comfort food.
+            <p className='flex justify-center tracking-widest text-mainWhite text-xl font-bold'>
+                WHERE LATE-NIGHT CRAVINGS MEET IRRESISTIBLE COMFORT FOOD.
             </p>
             <div className='flex flex-row space-x-4 justify-center mt-20'>
                 <YellowButtonHollow text='View Our Bar' navigateTo='/drinks' />
