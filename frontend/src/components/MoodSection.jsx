3117df7ae0f6be61cc251d0642fc67b0a3176687
marshall nickolauson <marshnickol101@gmail.com>
2024-10-25 13:34:30 -0400

    updated styling

--- frontend/src/components/MoodSection.jsx
+++ frontend/src/components/MoodSection.jsx
@@ -1,6 +1,6 @@
-import peopleEating from '../assets/img/people-eating.jpeg'
-import takeoutMeal from '../assets/img/takeout-meal.jpg'
-import YellowButtonFilled from './YellowButtonFilled'
+import peopleEating from '../assets/img/people-eating.jpeg';
+import takeoutMeal from '../assets/img/takeout-meal.jpg';
+import YellowButtonFilled from './YellowButtonFilled';
 
 const MoodSection = () => {
     return (
@@ -11,26 +11,31 @@ const MoodSection = () => {
                     <h1 className='text-mainYellow'>MOOD?</h1>
                 </div>
             </div>
-            <div className='flex flex-row justify-center space-x-6 text-mainWhite'>
-                <div className='flex flex-row items-center space-x-4 bg-mainDarkGray p-4 rounded-lg white-shadow'>
+            <div className='flex flex-row justify-center space-x-4 text-mainWhite'>
+                {/* Left Block: Dine-In */}
+                <div className='flex flex-row items-center space-x-6 p-4 text-right'>
                     <img className="w-64 h-64 object-cover rounded-lg" src={peopleEating} alt="Dine-In Image" />
-                    <div className='flex flex-col text-mainWhite items-center text-center space-y-8'>
+                    <div className='flex flex-col items-end text-mainWhite space-y-8'>
                         <h1 className='font-bold tracking-wider text-3xl'>Dine-In</h1>
-                        <p className='text-lg'>Join us for a cozy night out.</p>
-                        <YellowButtonFilled text='Reserve a Table' navigateTo='/reservations' width='250' />
+                        <p className='text-lg'>For a cozy night out.</p>
+                        <YellowButtonFilled text='Reserve a Table' navigateTo='/reservations' width='200' />
                     </div>
                 </div>
-                <div className='flex flex-row items-center space-x-4 bg-mainDarkGray p-4 rounded-lg white-shadow'>
-                    <div className='flex flex-col text-mainWhite items-center text-center space-y-8'>
+
+                <div className='border-r-2 border-mainWhite'></div>
+
+                {/* Right Block: Takeout */}
+                <div className='flex flex-row items-center space-x-6 p-4 text-left'>
+                    <div className='flex flex-col items-start text-mainWhite space-y-8'>
                         <h1 className='font-bold tracking-wider text-3xl'>Takeout</h1>
-                        <p className='text-lg'>Grab your favorites to-go!</p>
-                        <YellowButtonFilled text='Order Now' navigateTo='/order' />
+                        <p className='text-lg'>For your favorites to-go.</p>
+                        <YellowButtonFilled text='Order Now' navigateTo='/order' width='200' />
                     </div>
                     <img className="w-64 h-64 object-cover rounded-lg" src={takeoutMeal} alt="Takeout Image" />
                 </div>
             </div>
-        </section >
-    )
+        </section>
+    );
 }
 
-export default MoodSection
+export default MoodSection;
--- frontend/src/components/YellowButtonFilled.jsx
+++ frontend/src/components/YellowButtonFilled.jsx
@@ -7,20 +7,21 @@ const YellowButtonFilled = ({ text = 'blank', navigateTo = '/', width = '150' })
   const buttonClick = () => {
     console.log(text);
     navigate(`${navigateTo}`);
-  }
+  };
 
   return (
     <div
       onClick={buttonClick}
-      className={`bg-mainYellow w-[${width}px] hover:bg-transparent hover:border-mainYellow-500 hover:text-mainYellow cursor-pointer border-4 border-mainYellow transition-all duration-300 ease-in-out group`}
+      style={{ width: `${width}px` }}
+      className="bg-mainYellow hover:bg-transparent hover:border-mainYellow-500 hover:text-mainYellow cursor-pointer border-4 border-mainYellow transition-all duration-300 ease-in-out group"
     >
-      <div className={`pt-3 pb-3 text-center ${width > 200 ? 'px-3' : ''}`}>
+      <div className="pt-3 pb-3 text-center">
         <h1 className="text-mainBlack tracking-wider font-bold text-lg transition-all duration-200 ease-in-out group-hover:text-mainYellow">
           {text}
         </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default YellowButtonFilled;
