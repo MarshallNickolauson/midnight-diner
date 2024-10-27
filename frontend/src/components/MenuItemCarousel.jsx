6037e01996de92bb6206ad42017f116f75bcd24c
marshall nickolauson <marshnickol101@gmail.com>
2024-10-27 06:07:32 -0400

    added horizontal scrolling to swiper view

--- frontend/src/components/MenuItemCarousel.jsx
+++ frontend/src/components/MenuItemCarousel.jsx
@@ -1,19 +1,21 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
+import { Mousewheel } from 'swiper/modules';
 import MenuItemCard from './MenuItemCard';
 
 const MenuItemCarousel = ({ category, items }) => {
-    if (items.length === 0) return null;
+    if (items.length === 0) return null;    
     
     return (
         <div className="carousel-container overflow-hidden pb-6">
             <h2 className='text-xl text-mainBlack'>{category}</h2>
             <div className='border-b-2 border-mainBlack rounded-full mt-1 mb-4'></div>
             <Swiper
+                modules={[Mousewheel]}
                 spaceBetween={15}
                 slidesPerView={'auto'}
-                onSlideChange={() => console.log('Slide changed')}
+                mousewheel={{ forceToAxis: true, releaseOnEdges: true }}
             >
                 {items.map(item => (
                     <SwiperSlide key={item._id} style={{ width: 'auto' }}>
