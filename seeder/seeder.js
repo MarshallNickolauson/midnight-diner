0569f045da0bb4e86f3a1dea67e328c179770b86
marshall nickolauson <marshnickol101@gmail.com>
2025-01-01 19:22:31 -0500

    update: seeder includes default admin user

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -9,12 +9,13 @@ import colors from 'colors';
 import FormData from 'form-data';
 
 import MenuItem from './menuItemModel.js';
+import User from './userModel.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const UPLOAD_URL = 'http://backend:5000/upload';
-const COLLECTION_NAME = 'menu-items';
+const BACKEND_URL = 'http://backend:5000';
 const IMAGE_DIR = path.resolve(__dirname, 'images');
 const MENU_JSON_FILE = path.resolve(__dirname, 'menu-seeds.json');
 
@@ -24,15 +25,15 @@ const waitForMongo = async (timeout = 30000) => {
     while (Date.now() - start < timeout) {
         try {
             conn = await mongoose.connect(process.env.MONGO_URI);
-            console.log('[Menu Seeder] MongoDB is ready!'.cyan);
+            console.log('[Seeder] MongoDB is ready!'.cyan);
             break;
         } catch (err) {
-            console.log('[Menu Seeder] Waiting for MongoDB to be ready...');
+            console.log('[Seeder] Waiting for MongoDB to be ready...');
             await new Promise((resolve) => setTimeout(resolve, 2000));
         }
     }
     if (!conn) {
-        throw new Error('[Menu Seeder] MongoDB did not become ready within the timeout period.');
+        throw new Error('[Seeder] MongoDB did not become ready within the timeout period.');
     }
 };
 
@@ -41,24 +42,24 @@ const waitForBackend = async (timeout = 300000) => {
     let backendReady = false;
     while (Date.now() - start < timeout) {
         try {
-            await axios.get('http://backend:5000');
+            await axios.get(BACKEND_URL);
             backendReady = true;
-            console.log('[Menu Seeder] Backend is ready!');
+            console.log('[Seeder] Backend is ready!');
             break;
         } catch (err) {
-            console.log('[Menu Seeder] Waiting for backend to be ready...');
+            console.log('[Seeder] Waiting for backend to be ready...');
             await new Promise((resolve) => setTimeout(resolve, 2000));
         }
     }
     if (!backendReady) {
-        throw new Error('[Menu Seeder] Backend did not become ready within the timeout period.');
+        throw new Error('[Seeder] Backend did not become ready within the timeout period.');
     }
 };
 
 const uploadImage = async (imagePath) => {
     try {
         if (!fs.existsSync(imagePath)) {
-            console.error(`[Menu Seeder] Image file not found at path: ${imagePath}`);
+            console.error(`[Seeder] Image file not found at path: ${imagePath}`);
             return null;
         }
 
@@ -71,18 +72,18 @@ const uploadImage = async (imagePath) => {
             headers: formData.getHeaders(),
         });
 
-        console.log('[Menu Seeder] Image uploaded successfully.');
+        console.log('[Seeder] Image uploaded successfully.');
 
         return response.data.file.path;
     } catch (err) {
         // Log the error in detail
-        console.error(`[Menu Seeder] Failed to upload ${imagePath}: ${err.message}`);
+        console.error(`[Seeder] Failed to upload ${imagePath}: ${err.message}`);
         if (err.response) {
-            console.error('[Menu Seeder] Response error:', err.response.data);
+            console.error('[Seeder] Response error:', err.response.data);
         } else if (err.request) {
-            console.error('[Menu Seeder] No response received:', err.request);
+            console.error('[Seeder] No response received:', err.request);
         } else {
-            console.error('[Menu Seeder] Error in setting up request:', err.message);
+            console.error('[Seeder] Error in setting up request:', err.message);
         }
         return null;
     }
@@ -98,19 +99,19 @@ const populateMenu = async () => {
         const imagePath = path.join(IMAGE_DIR, item.image_file);
 
         if (!fs.existsSync(imagePath)) {
-            console.error(`[Menu Seeder] Image file not found: ${imagePath}`);
+            console.error(`[Seeder] Image file not found: ${imagePath}`);
             continue;
         }
 
-        console.log(`[Menu Seeder] Uploading image: ${item.image_file}`);
+        console.log(`[Seeder] Uploading image: ${item.image_file}`);
         const imageUrl = await uploadImage(imagePath);
 
         if (!imageUrl) {
-            console.error(`[Menu Seeder] Failed to upload image for ${item.name}`);
+            console.error(`[Seeder] Failed to upload image for ${item.name}`);
             continue;
         }
 
-        console.log(`[Menu Seeder] Image URL: ${imageUrl}`);
+        console.log(`[Seeder] Image URL: ${imageUrl}`);
 
         const menuItem = new MenuItem({
             name: item.name,
@@ -126,14 +127,57 @@ const populateMenu = async () => {
         });
 
         await menuItem.save();
-        console.log(`[Menu Seeder] Inserted: ${item.name}`);
+        console.log(`[Seeder] Inserted: ${item.name}`);
     }
 
-    console.log("[Menu Seeder] Menu population complete.");
+    console.log("[Seeder] Menu population complete.");
+
     mongoose.disconnect();
-    console.log("[Menu Seeder] Disconnected seeder to MongoDB");
+    console.log("[Seeder] Disconnected seeder to MongoDB");
+}
+
+const populateAdminUser = async () => {
+    console.log("[Seeder] Populating database with default admin user...");
+    await waitForMongo();
+    await waitForBackend();
+
+    const formData = {
+        name: 'admin',
+        email: 'admin@email.com',
+        password: 'password',
+        phone: '1234567890'
+    };
+
+    const adminUserExists = await User.findOne({ email: 'admin@email.com'});
+
+    if (!adminUserExists) {
+        await axios.post(`${BACKEND_URL}/api/users/register`, formData, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+
+    const adminUser = await User.findOne({ email: 'admin@email.com'});
+
+    if (adminUser) {
+        adminUser.isAdmin = true
+        
+        const updatedUser = await adminUser.save();
+
+        setTimeout(() => {
+            console.log(updatedUser);
+        }, 3000);
+    }
+
+    console.log("[Seeder] Disconnected seeder to MongoDB");
 }
 
 populateMenu().catch((err) => {
     console.error("Error populating menu:", err.message);
 });
+
+populateAdminUser().catch((err) => {
+    console.error("Error populating admin user:", err.message);
+});
+
